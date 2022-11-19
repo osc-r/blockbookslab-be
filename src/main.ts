8f6f928dd5a09c07138fe84aac62d4f2272b842f
Chanatip R <chanatip.rassameecharerntham@scb.co.th>
2022-11-20 04:48:35 +0700

    add: ssl

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './helper/transform.interceptor';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('certificates/key.pem'),
+      cert: fs.readFileSync('certificates/cert.pem'),
+    },
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
